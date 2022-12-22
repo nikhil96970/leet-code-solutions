366dd3dc4a5526a21207affc51d9c429c290148b
Telukuntha Nikhil <83576138+nikhil96970@users.noreply.github.com>
2022-12-22 13:40:56 +0530

    Create 2011. Final Value of Variable After Performing Operations.py

---
+++ 2011. Final Value of Variable After Performing Operations.py
@@ -0,0 +1,10 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        n=0
+        i=0
+        for i in operations:
+            if(i=="--X" or i=="X--"):
+                n-=1
+            elif(i=="++X" or i=="X++"):
+                n+=1
+        return n
