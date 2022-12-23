83036bcba8a99e6b470237ec525778aa875c2b73
Telukuntha Nikhil <83576138+nikhil96970@users.noreply.github.com>
2022-12-23 13:17:32 +0530

    Create 1470. Shuffle the Array.py

---
+++ 1470. Shuffle the Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+        n = len(nums) // 2
+        for i in range(n):
+            res.append(nums[i])
+            res.append(nums[n + i])
+        return res
